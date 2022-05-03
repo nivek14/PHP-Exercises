8611433c57a5a328c35aa48da2d6bd0d2e35a860
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2022-05-03 16:08:28 -0300

    Alura - Avançando no PHP

---
+++ Alura-PHP/Avançando-com-PHP/contas_correntes.php
@@ -0,0 +1,22 @@
+<?php
+
+    $conta1 = [
+        "titular" => "teste",
+        "saldo" => 1000.00
+    ];
+
+    $conta2 = [
+        "titular" => "teste2",
+        "saldo" => 1020.00
+    ];
+
+    $conta3 = [
+        "titular" => "teste3",
+        "saldo" => 1300.00
+    ];
+
+    $contas = [$conta1, $conta2, $conta3];
+
+    foreach($contas as $conta){
+        echo $conta["titular"] . PHP_EOL;
+    }
---
+++ Alura-PHP/Avançando-com-PHP/listas.php
@@ -0,0 +1,8 @@
+<?php
+
+    $idades = [21,33,19,25,41];
+    $primeira = $idades[0];
+
+    for($i=0;$i<count($idades);$i++){
+        echo $idades[$i] . PHP_EOL;
+    }
